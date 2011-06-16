ce23c2b66dc141a7d836fa16f227e67658547802
Ianaré Sévi <ianare@gmail.com>
2011-06-16 19:03:35 +0200

    initial import

---
+++ JQRelcopy.php
@@ -0,0 +1,107 @@
+<?php
+
+/**
+ * JQRelcopy.php
+ *
+ * A wrapper for the jquery plugin 'relCopy'
+ *
+ * @link http://www.andresvidal.com/labs/relcopy.html
+ *
+ * @author Joe Blocher <yii@myticket.at>
+ * @copyright 2011 myticket it-solutions gmbh
+ * @license New BSD License
+ * @category User Interface
+ * @version 1.1
+ */
+class JQRelcopy extends CWidget
+{
+	/**
+	 * The text for the remove link
+	 * Can be an image tag too.
+	 * Leave empty to disable removing.
+	 *
+	 * @var string $removeText
+	 */
+	public $removeText;
+
+	/**
+	 * The htmlOptions for the remove link
+	 *
+	 * @var array $removeHtmlOptions
+	 */
+	public $removeHtmlOptions = array();
+
+	/**
+	 * Available options
+	 *
+	 * string excludeSelector - A jQuery selector used to exclude an element and its children
+	 * integer limit - The number of allowed copies. Default: 0 is unlimited
+	 * string append - Additional HTML to attach at the end of each copy.
+	 * string copyClass - A class to attach to each copy
+	 * boolean clearInputs - Option to clear each copies text input fields or textarea
+	 *
+	 * @var array $options
+	 */
+	public $options = array();
+
+	/**
+	 * The javascript code jsBeforeClone,jsAfterClone ...
+	 * This allows to handle widgets on cloning.
+	 * Important: 'this' is the current handled jQuery object
+	 *
+	 */
+	public $jsBeforeClone; // 'jsBeforeClone' => "alert(this.attr('class'));";
+	public $jsAfterClone;  // 'jsAfterClone' => "alert(this.attr('class'));";
+	public $jsBeforeNewId;  // 'jsBeforeNewId' => "alert(this.attr('id'));";
+	public $jsAfterNewId;  // 'jsAfterNewId' => "alert(this.attr('id'));";
+	public $jsFinish;  // 'jsFinish' => "alert(this.attr('class'));";
+
+	/**
+	 * The assets url
+	 *
+	 * @var string $_assets
+	 */
+	private $_assets;
+
+	/**
+	 * Initialize the widget
+	 *
+	 * @return
+	 */
+	public function init()
+	{
+		$this->_assets = Yii::app()->assetManager->publish(dirname(__FILE__).DIRECTORY_SEPARATOR.'assets');
+
+		Yii::app()->clientScript->registerCoreScript('jquery')
+		          ->registerScriptFile($this->_assets.'/js/jquery.relcopy.1.1.js');
+
+		if (!empty($this->removeText))
+		{
+			$onClick = '$(this).parent().remove(); return false;';
+			$htmlOptions = array_merge(array('onclick'=>$onClick),$this->removeHtmlOptions);
+			$append = CHtml::link($this->removeText,'#',$htmlOptions);
+
+			$this->options['append'] = empty($this->options['append']) ? $append : $append .' '.$this->options['append'];
+		}
+
+		if (!empty($this->jsBeforeClone))
+			$this->options['beforeClone'] = $this->jsBeforeClone;
+
+		if (!empty($this->jsAfterClone))
+			$this->options['afterClone'] = $this->jsAfterClone;
+		
+		if (!empty($this->jsBeforeNewId))
+			$this->options['beforeNewId'] = $this->jsBeforeNewId;
+
+		if (!empty($this->jsAfterNewId))
+			$this->options['afterNewId'] = $this->jsAfterNewId;
+		
+		if (!empty($this->jsFinish))
+			$this->options['finish'] = $this->jsFinish;
+
+		$options = CJavaScript::encode($this->options);
+		Yii::app()->getClientScript()->registerScript(__CLASS__.'#'.$this->id,"jQuery('#{$this->id}').relCopy($options);");
+		parent::init();
+	}
+}
+?>
---
+++ assets/js/jquery.relcopy.1.1.js
@@ -0,0 +1,175 @@
+/**
+ * jQuery-Plugin "relCopy"
+ *
+ * @version: 1.1.0, 25.02.2010
+ *
+ * @author: Andres Vidal
+ *          code@andresvidal.com
+ *          http://www.andresvidal.com
+ *
+ * Instructions: Call $(selector).relCopy(options) on an element with a jQuery type selector
+ * defined in the attribute "rel" tag. This defines the DOM element to copy.
+ * @example: $('a.copy').relCopy({limit: 5}); // <a href="example.com" class="copy" rel=".phone">Copy Phone</a>
+ *
+ * @param: string	excludeSelector - A jQuery selector used to exclude an element and its children
+ * @param: integer	limit - The number of allowed copies. Default: 0 is unlimited
+ * @param: string	append - HTML to attach at the end of each copy. Default: remove link
+ * @param: string	copyClass - A class to attach to each copy
+ * @param: boolean	clearInputs - Option to clear each copies text input fields or textarea
+ *
+ *
+ * Modified by Ianaré Sévi to integrate better with Yii models.
+ */
+
+(function($) {
+
+	// to count IDs
+	var idCounter = 1;
+
+	// create an ID suitable for a new insert in the DB
+	function getIdCount(c){
+		return Array(c+1).join("0");
+	}
+
+	// Clear names on elements to reflect the fact they will need insertion to
+	// the DB
+	function resetName(element){
+		var re = new RegExp(/\[\d+\](?=\[)/);
+		return element.attr('name').replace(re, "[" + getIdCount(idCounter) + "]");
+	}
+	
+	// Clear ids on elements to reflect the fact they will need insertion to
+	// the DB
+	function resetId(element, counter){
+		var re = new RegExp(/_\d/);
+		if (element.attr('id').match(re)){
+			return element.attr('id').replace(re, "_" + getIdCount(idCounter));
+		}
+		else {
+			return element.attr('id') + (counter +1);
+		}
+	}
+	
+	$.fn.relCopy = function(options) {
+		var settings = jQuery.extend({
+			excludeSelector: ".exclude",
+			replaceSelector: null,
+			emptySelector: ".empty",
+			copyClass: "copy",
+			append: '',
+			clearInputs: true,
+			noClearSelector: ".noClear",
+			withDataAndEvents: true,
+			limit: 0, // 0 = unlimited
+			beforeClone: null,
+			afterClone: null,
+			beforeNewId: null,
+			afterNewId: null,
+			finish: null
+		}, options);
+
+		settings.limit = parseInt(settings.limit);
+
+		// apply click function to id
+		$('body').delegate("#"+$(this).attr('id'),"click", function(){
+
+				var rel = $(this).attr('rel'); // rel in jquery selector format
+				var counter = $(rel).length;
+				idCounter++;
+
+				// stop limit
+				if (settings.limit != 0 && counter >= settings.limit){
+					return false;
+				};
+				
+				var funcBeforeClone = function(){eval(settings.beforeClone);};
+				var funcAfterClone = function(){eval(settings.afterClone);};
+				var funcBeforeNewId = function(){eval(settings.beforeNewId);};
+				var funcAfterNewId = function(){eval(settings.afterNewId);};
+				var funcFinish = function(){eval(settings.finish);};
+
+				var master = $(rel+":first");
+				funcBeforeClone.call(master);
+				
+				var parent = $(master).parent();
+				var clone = $(master).clone(settings.withDataAndEvents).addClass(settings.copyClass+counter);
+				funcAfterClone.call(clone);
+				
+				// replace elements with replaceSelector
+				if (settings.replaceSelector) {
+					clone.find(settings.replaceSelector).replaceWith(settings.append);
+				}
+				else {
+					clone.append(settings.append);
+				}
+				
+				// reset datepicker
+				$(clone).find(':input').removeClass("hasDatepicker");
+
+				//Remove Elements with excludeSelector
+				if (settings.excludeSelector){
+					$(clone).find(settings.excludeSelector).remove();
+				};
+
+				//Empty Elements with emptySelector
+				if (settings.emptySelector){
+					$(clone).find(settings.emptySelector).empty();
+				};
+
+				// Increment Clone IDs
+				if ( $(clone).attr('id') ){
+					funcBeforeNewId.call(clone);
+					$(clone).attr('id', resetId($(clone), counter));
+					funcAfterNewId.call(clone);
+				};
+				// Increment Clone Children IDs
+				$(clone).find('[id]').each(function(){
+					funcBeforeNewId.call($(this));
+					$(this).attr('id', resetId($(this), counter));
+					funcAfterNewId.call($(this));
+				});
+
+				// Increment Clone names
+				if ( $(clone).attr('name') ){
+					$(clone).attr('name', resetName($(clone)));
+				};
+				// Increment Clone Children names
+				$(clone).find('[name]').each(function(){
+					$(this).attr('name', resetName($(this)));
+				});
+				
+				funcAfterNewId.call();
+
+				//Clear Inputs/Textarea
+				if (settings.clearInputs){
+					var inputs = $(clone).find(':input:not("'+settings.noClearSelector+'")');
+					inputs.each(function(){
+						var type = $(this).attr('type');
+						switch(type)
+						{
+							case "button":
+								break;
+							case "reset":
+								break;
+							case "submit":
+								break;
+							case "checkbox":
+								$(this).attr('checked', '');
+								break;
+							default:
+								$(this).val("");
+						}
+					});
+				};
+
+				$(parent).find(rel+':last').after(clone);
+
+				funcFinish.call(clone);
+				return false;
+
+		}); // end click action
+
+		return this; // return to jQuery
+	};
+
+})(jQuery);
